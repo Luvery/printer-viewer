570fa21d6d8a569e6921005417859de3ebb1eb3c
Luvery <TestName@example.com>
2022-03-06 22:33:00 +0100

    fixes and new styling

--- printers-app/src/components/Printer.js
+++ printers-app/src/components/Printer.js
@@ -13,23 +13,25 @@ function Printer(props) {
   const printerData = props;
   const color = printerData.data.color ? <p>kolorowa</p> : <p>mono</p>;
   return (
-    <div className='listItem'>
-      <img src={printerData.data.picture} alt='logo' className='picture' />
-      <h1>{printerData.data.brand}</h1>
-      <h2 className='model'>{printerData.data.model.toUpperCase()}</h2>
-      <p>{printerData.data.type}</p>
-      {color}
-      <h3>
-        {printerData.data.price}
-        zł *
-      </h3>
-      <p>
-        <FontAwesomeIcon icon={faGlobe} />
-        <a className='App-link' href={printerData.data.website} target='_blank' rel='noopener noreferrer'>
-          {' '}
-          Strona producenta
-        </a>
-      </p>
+    <div className='card'>
+      <div className='card-body'>
+        <img src={printerData.data.picture} alt='logo' className='picture' />
+        <h1>{printerData.data.brand}</h1>
+        <h2 className='model'>{printerData.data.model.toUpperCase()}</h2>
+        <p>{printerData.data.type}</p>
+        {color}
+        <h3>
+          {printerData.data.price}
+          zł *
+        </h3>
+        <p>
+          <FontAwesomeIcon icon={faGlobe} />
+          <a className='app-link' href={printerData.data.website} target='_blank' rel='noopener noreferrer'>
+            {' '}
+            Strona z drukarką
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
